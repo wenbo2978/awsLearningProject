a95816a152bd1776a63a4535e5a098838b8b85d6
wenboC <wenbo.chen2022@gmail.com>
2026-01-22 09:58:52 -0500

    Update placeOrder MQ

--- src/main/java/com/aws_practice/rabbitmq/order/OrderPlacedDomainEventHandler.java
+++ src/main/java/com/aws_practice/rabbitmq/order/OrderPlacedDomainEventHandler.java
@@ -2,6 +2,7 @@ package com.aws_practice.rabbitmq.order;
 
 import com.aws_practice.dto.OrderPlacedDomainEvent;
 import com.aws_practice.dto.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -9,12 +10,9 @@ import org.springframework.transaction.event.TransactionalEventListener;
 import java.time.Instant;
 
 @Component
+@RequiredArgsConstructor
 public class OrderPlacedDomainEventHandler {
     private final OrderEventPublisher rabbitPublisher;
-
-    public OrderPlacedDomainEventHandler(OrderEventPublisher rabbitPublisher) {
-        this.rabbitPublisher = rabbitPublisher;
-    }
 
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(OrderPlacedDomainEvent e) {
